8686a7807645a8c88cc6a3d77aa868df5f8d7760
tls9603-web <tls9603@gmail.com>
2025-12-08 21:35:54 +0900

    outgame : UI 및 Controller 클래스 생성

---
+++ Source/Team03/Outgame/UW_LobbyLevel.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UW_LobbyLevel.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEAM03_API UUW_LobbyLevel : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Team03/Outgame/UW_TitleLevel.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UW_TitleLevel.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEAM03_API UUW_TitleLevel : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
