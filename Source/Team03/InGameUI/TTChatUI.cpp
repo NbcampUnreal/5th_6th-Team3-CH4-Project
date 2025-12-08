e537d09570b7f7817c75fd3ac12e22345113f98b
ParkChanYeong <sidsru21131210@gmail.com>
2025-12-08 20:14:16 +0900

    Add : 채팅 UI추가

---
+++ Source/Team03/InGameUI/TTChatUI.cpp
@@ -0,0 +1,64 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TTChatUI.h"
+#include "Components/EditableTextBox.h"
+#include "Components/ScrollBox.h"
+
+void UTTChatUI::NativeConstruct ()
+{
+	Super::NativeConstruct ();
+	if (IsValid ( ChatInputBox ) )
+	{
+		ChatInputBox->OnTextCommitted.AddDynamic ( this , &UTTChatUI::OnTextCommitted );
+	}
+	ChatInputBox->SetIsEnabled ( false );
+}
+
+void UTTChatUI::NativeDestruct ()
+{
+	Super::NativeDestruct ();
+	if (IsValid ( ChatInputBox ) )
+	{
+		ChatInputBox->OnTextCommitted.RemoveDynamic ( this , &UTTChatUI::OnTextCommitted );
+	}
+}
+
+void UTTChatUI::SetChatMessage ( const FString& Message )
+{
+	if (IsValid(ChatInputBox))
+	{
+		ChatInputBox->SetText ( FText::FromString ( Message ) );
+	}
+}
+void UTTChatUI::ActivateChat ()
+{
+	if (IsValid ( ChatInputBox ) )
+	{
+		ChatInputBox->SetIsEnabled ( true );
+		ChatInputBox->SetKeyboardFocus ();
+	}
+}
+void UTTChatUI::ServerRPCPrintString_Implementation ( const FString& InMessage )
+{
+	MulticastRPCPrintString ( InMessage );
+}
+void UTTChatUI::MulticastRPCPrintString_Implementation ( const FString& InMessage )
+{
+}
+void UTTChatUI::OnTextCommitted ( const FText& Text , ETextCommit::Type CommitMethod )
+{
+	if (CommitMethod == ETextCommit::OnEnter)
+	{
+		if (IsValid( ChatInputBox))
+		{
+			FText InputText = ChatInputBox->GetText ();
+			FString Message = InputText.ToString ().TrimStartAndEnd ();
+
+			if(!Message.IsEmpty())
+			{
+				ServerRPCPrintString( Message );
+			}
+		}
+	}
+}
---
+++ Source/Team03/InGameUI/TTChatUI.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "TTChatUI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEAM03_API UTTChatUI : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	virtual void NativeConstruct () override;
+	virtual void NativeDestruct () override;
+
+	void SetChatMessage ( const FString& Message );
+
+	UFUNCTION()
+	void ActivateChat();
+
+	UFUNCTION ( Server , Reliable )
+	void ServerRPCPrintString ( const FString& InMessage );
+	UFUNCTION ( NetMulticast , Reliable )
+	void MulticastRPCPrintString ( const FString& InMessage );
+protected:
+	UFUNCTION()
+	void OnTextCommitted ( const FText& Text , ETextCommit::Type CommitMethod );
+
+private:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<class UEditableTextBox> ChatInputBox;
+
+	UPROPERTY ( meta = (BindWidget) )
+	TObjectPtr<class UScrollBox> ChatScrollBox;
+};
