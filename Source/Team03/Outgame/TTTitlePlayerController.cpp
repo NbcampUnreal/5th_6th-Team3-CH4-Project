4378124302dca9ed17fddf03cface1b34e79e11f
tls9603-web <tls9603@gmail.com>
2025-12-09 17:45:22 +0900

    add : 타이틀 레벨 프로토타입 구현

--- Source/Team03/Outgame/TTTitlePlayerController.cpp
+++ Source/Team03/Outgame/TTTitlePlayerController.cpp
@@ -1,5 +1,19 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "TTTitlePlayerController.h"
 
+ATTTitlePlayerController::ATTTitlePlayerController()
+{
+	// SRS 4.2: Mouse Cursor Show
+	bShowMouseCursor = true;
+	bEnableClickEvents = true;
+	bEnableMouseOverEvents = true;
+}
+
+void ATTTitlePlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	SetInputMode(FInputModeUIOnly());
+}
