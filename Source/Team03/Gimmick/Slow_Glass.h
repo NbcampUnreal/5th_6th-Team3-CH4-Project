49dd07f528704c5b02da29af68ef19fafbb38a24
zerg8474 <zerg8474@gmail.com>
2025-12-16 11:49:01 +0900

    임시 투척류

---
+++ Source/Team03/Gimmick/Slow_Glass.h
@@ -0,0 +1,26 @@
+﻿//Slow_Glass.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Gimmick/Glass_Bottle.h"
+#include "Slow_Glass.generated.h"
+
+UCLASS ()
+class TEAM03_API ASlow_Glass : public AGlass_Bottle
+{
+	GENERATED_BODY ()
+
+public:
+	ASlow_Glass ();
+
+protected:
+
+	UPROPERTY ( EditAnywhere , BlueprintReadWrite , Category = "Slow" )
+	float SlowAmount;
+
+	UPROPERTY ( EditAnywhere , BlueprintReadWrite , Category = "Slow" )
+	float SlowDuration;
+
+	virtual void Explode_Implementation () override;
+};
