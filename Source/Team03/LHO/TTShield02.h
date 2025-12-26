c2b4212ad18007336e5ddcf65173f8e28518bd09
GOLDEGG99 <kevinhook@naver.com>
2025-12-26 21:56:21 +0900

    호욱
    쉴드2, 무기 3종류 추가/멀티플레이 화면 공격 모션 구현

---
+++ Source/Team03/LHO/TTShield02.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TTShield02.generated.h"
+
+class ATTPlayerCharacter;
+class UTTPickupComponent;
+
+UCLASS()
+class TEAM03_API ATTShield02 : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+    ATTShield02 ();
+
+UPROPERTY ( EditAnywhere , Category = "Weapon" )
+FName WeaponRowName;
+
+UTTPickupComponent* GetPickupComponent () const { return PickupComponent; }
+
+UFUNCTION ()
+void HandleOnThrowAway ();
+
+protected:
+	virtual void BeginPlay () override;
+
+	UFUNCTION ()
+	void HandleOnPickUp ( ATTPlayerCharacter* InPickUpCharacter );
+
+protected:
+	UPROPERTY ( EditDefaultsOnly , BlueprintReadOnly )
+	TObjectPtr<UTTPickupComponent> PickupComponent;
+
+};
